223d450e3048348538aed049e8c9d5df2c469ce1
Ruveyda-Kışla <ruveydakisla572@gmail.com>
2023-09-24 03:11:27 +0330

    chore: add font and colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,29 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/blocks/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        WhiteSmoke: '#F5F5F5',
+        BabyBlue: '#E4F6F9',
+        MidnightBlue: '#050142',
+        RoyalPurple: '#3A21D4',
+        LemonYellow: '#FCFF56',
+        LightSkyBlue: '#75EEFF',
+      },
+      screens: {
+        sm: { max: '767px' },
+        md: { min: '768px', max: '1023px' },
+        lg: { min: '1024px', max: '1279px' },
+        xl: { min: '1280px', max: '1535px' },
+        xxl: { min: '1536px' },
+      },
+    },
   },
   plugins: [],
 };
+
 export default config;
