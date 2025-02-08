a741cc8b067adf8c897b922cbf3ba4f82908966a
dousdogukan <dousdogukan@gmail.com>
2025-02-08 22:54:39 +0330

    temp: update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,8 @@ export const metadata: Metadata = {
 
 const myFont = localFont({
   src: [
-    { style: 'normal', weight: '400', path: '../../public/font/Aeonik-Regular.ttf' },
-    { style: 'normal', weight: '500', path: '../../public/font/Aeonik-Medium.ttf' },
-    { style: 'normal', weight: '700', path: '../../public/font/Aeonik-Bold.ttf' },
+    { style: 'normal', weight: '400', path: '../../public/font/Aeonik-Regular.woff' },
+    { style: 'normal', weight: '500', path: '../../public/font/Aeonik-Medium.woff' },
   ],
 });
 
